a786988354f205bad02a1a3385866215add34aeb
Atikur Rahman <atik.bd08@gmail.com>
2016-06-01 19:47:41 +0600

    add TravelLocationsMapViewController

---
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TravelLocationsMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Atikur Rahman on 6/1/16.
+//  Copyright © 2016 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+}
