1d73095c3f33d5126af63b2dacfa6a0b7be9c28b
Atikur Rahman <atik.bd08@gmail.com>
2016-06-03 22:51:09 +0600

    download photos

--- VirtualTourist/PhotoAlbumCollectionViewController.swift
+++ VirtualTourist/PhotoAlbumCollectionViewController.swift
@@ -12,10 +12,32 @@ import MapKit
 class PhotoAlbumCollectionViewController: UIViewController {
     
     var annoation: MKAnnotation!
+    var isPhotoAlbumAvailable = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(annoation.coordinate)
+        if isPhotoAlbumAvailable {
+            getSavedPhotos()
+        } else {
+            downloadPhotos()
+        }
+    }
+    
+    func downloadPhotos() {
+        FlickrClient.sharedInstance.getPhotosForLocation(annoation.coordinate) {
+            photoURLs, errorString in
+            
+            guard let photoURLs = photoURLs else {
+                print("Error: \(errorString)")
+                return
+            }
+            
+            print("Total photos: \(photoURLs.count)")
+        }
+    }
+    
+    func getSavedPhotos() {
+        print("get previously saved photos")
     }
 }
